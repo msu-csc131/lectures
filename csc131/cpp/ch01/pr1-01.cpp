4883bd5429444c8663f7aa2a30b7425d362b755c
Mr. Daehn <jdaehn@missouristate.edu>
2018-03-21 21:50:29 -0500

    LECTURES-DEVELOP - Update gitignore; ignore exe and out files.

--- csc131/cpp/ch01/pr1-01.cpp
+++ csc131/cpp/ch01/pr1-01.cpp
@@ -15,20 +15,20 @@
  */
 int main()
 {
-  double hours, rate, pay;
-  
-  // Get the number of hours worked.
-  std::cout << "How many hours did you work? ";
-  std::cin  >> hours;
-  
-  // Get the hourly pay rate.
-  std::cout << "How much do you get paid per hour? ";
-  std::cin  >> rate;
-  
-  // Calculate the pay.
-  pay = hours * rate;
-  
-  // Display the pay.
-  std::cout << "You have earned $" << pay << std::endl;
-  return EXIT_SUCCESS;
+    double hours, rate, pay;
+    
+    // Get the number of hours worked.
+    std::cout << "How many hours did you work? ";
+    std::cin  >> hours;
+    
+    // Get the hourly pay rate.
+    std::cout << "How much do you get paid per hour? ";
+    std::cin  >> rate;
+    
+    // Calculate the pay.
+    pay = hours * rate;
+    
+    // Display the pay.
+    std::cout << "You have earned $" << pay << std::endl;
+    return EXIT_SUCCESS;
 }
--- csc131/cpp/ch01/pr1-02.cpp
+++ csc131/cpp/ch01/pr1-02.cpp
@@ -1,10 +1,21 @@
-// This program prints a message with your name in it. 
+/**
+ * CSC131 - Computational Thinking
+ * Missouri State University, Spring 2018
+ *
+ * @file pr1-02.cpp
+ * @authors Gaddis, et. al. 
+ */
+ 
+#include <cstdlib>
 #include <iostream>
-using namespace std;
 
+/**
+ * This program prints a message with your name in it.
+ * @return EXIT_SUCCESS is returned upon successful execution.
+ */
 int main()
 {
-   cout << "Hi! It\'s me.\n";
-   cout << "I\'m learning to program!\n";
-   return 0;
+    std::cout << "Hi! It\'s me.\n";
+    std::cout << "I\'m learning to program!\n";
+    return EXIT_SUCCESS;
 }
