5c8d11d001dbf0371e5cc3a5e3d5962a1343cf03
Mr. Daehn <jdaehn@missouristate.edu>
2018-03-21 21:28:10 -0500

    LECTURES-DEVELOP - Initial import of ch01 examples.

---
+++ csc131/cpp/ch01/pr1-02.cpp
@@ -0,0 +1,10 @@
+// This program prints a message with your name in it. 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hi! It\'s me.\n";
+   cout << "I\'m learning to program!\n";
+   return 0;
+}
